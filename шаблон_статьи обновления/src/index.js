830b20d091f5d1cbc34560dbc8b2ddc629adeba4
Nastya <n_m_666@mail.ru>
2022-02-03 11:55:52 +0500

    added test for bussines and article update

---
+++ шаблон_статьи обновления/src/index.js
@@ -0,0 +1,7 @@
+import "./style.scss"
+import "./scripts/scroll"
+import "./scripts/consultant"
+import "./scripts/reproduction"
+import "./scripts/data"
+import "./scripts/convead"
+import "./scripts/metrics"
---
+++ шаблон_статьи обновления/src/scripts/convead.js
@@ -0,0 +1,21 @@
+
+window.ConveadSettings = {
+    /* Use only [0-9a-z-] characters for visitor uid!
+    visitor_uid: '12345678',
+    visitor_info: {
+        first_name: 'John',
+        last_name: 'Smith',
+        email: 'john_smith@example.com',
+        phone: '8-800-000-000',
+        date_of_birth: '1974-07-30',
+        gender: 'male',
+        my_custom_numeric_property: 1234,
+        my_custom_string_property: 'foo',
+        my_custom_date_property: '2014-04-20',
+        my_custom_boolean_property: 'true'
+    }, */
+    app_key: "0941b6bf65402f9f664c3e3d675fd5ee"
+};
+
+(function (w, d, c) { w[c] = w[c] || function () { (w[c].q = w[c].q || []).push(arguments) }; var ts = (+new Date() / 86400000 | 0) * 86400; var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.charset = 'utf-8'; s.src = 'https://tracker.convead.io/widgets/...s'; var x = d.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); })(window, document, 'convead');
+
